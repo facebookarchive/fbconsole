05794a6ebf7904e15d01c84f39315ab3351e72d5
Paul Carduner <paul@carduner.net>
2012-01-15 02:34:38 -0800

    update copyright header

--- src/fbconsole.py
+++ src/fbconsole.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2010 Facebook
+# Copyright 2010-present Facebook
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
